22ddc92df8be2e3fd90a9ff9dbebd693b339d6a4
Tulun <jaykiraly@gmail.com>
2016-02-12 15:19:10 -0800

    switched from error template to package.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -14,11 +14,11 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {
       // display the error to the user and abort
       if (error)
-        return throwError(error.reason);
+        return Errors.throw(error.reason);
 
       // Show this result but route anyway
       if (result.postExists)
-        throwError('This link has already been posted.');
+        Errors.throw('This link has already been posted.');
       
       Router.go('postPage', {_id: result._id});
     });
